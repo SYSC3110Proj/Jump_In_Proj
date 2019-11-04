793a915b59223e4780745e421196e8044bb37762
Craig Worthington <craigworthington@LABS.SCE.CARLETON.CA>
2019-11-04 14:00:52 -0500

    Reworked controller to use points

--- Jump_In_Game/src/mvc/Controller.java
+++ Jump_In_Game/src/mvc/Controller.java
@@ -1,6 +1,7 @@
 package mvc;
 
 import java.awt.BorderLayout;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -15,6 +16,7 @@ public class Controller {
 	private View view;
 	boolean select;
 	private String loc, name;
+	private Point gridLocation;
 	
 	public Controller() {
 		game = new PlayBoard();
@@ -24,15 +26,18 @@ public class Controller {
 		
 		view.initButton(game.getBoardName(), new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(!select) {
+				if (!select) {
 					loc = ((JButton)e.getSource()).getName();
 					name = ((JButton)e.getSource()).getText();
+					
+					gridLocation = ((GridButton) e.getSource()).getGridLocation();
+					
 					//System.out.println(name + loc);
 					select = true;
-				}
-				else {
-					if(name != null) {
-						if(!name.equals("Hole") && !name.equals("mushroom")){
+				} else {
+					if (name != null) {
+						if (!name.equals("Hole") && !name.equals("mushroom")) {
+							
 							String[] str = loc.split(",");
 							int row1 = Integer.parseInt(str[0]);
 							int col1 = Integer.parseInt(str[1]);
@@ -40,19 +45,24 @@ public class Controller {
 							int col2 = Integer.parseInt(((JButton)e.getSource()).getName().split(",")[1]);
 						
 							if(name.equals("rabbit1") || name.equals("rabbit2") || name.equals("rabbit3")) {
-								game.jumpTo(game.getRabbit(name), getDirec(row1, col1, row2, col2));
-							}
-							else if(name.equals("fox1") || name.equals("fox2")) {
-								if(game.getFox(name)[0].getDirection().equals(Direction.HORIZONTAL)) {
+//								game.jumpTo(game.getRabbit(name), getDirec(row1, col1, row2, col2));
+								
+								game.jumpTo(game.getRabbit(name), getDirection(gridLocation, new Point(col2, row2)));
+								
+							} else if (name.equals("fox1") || name.equals("fox2")) {
+								if (game.getFox(name)[0].getDirection().equals(Direction.HORIZONTAL)) {
 									game.moveTo(game.getFox(name), col2);
+								} else {
+									game.moveTo(game.getFox(name), row2);
 								}
-								else game.moveTo(game.getFox(name), row2);
 							}
 							view.update(game.getBoardName());
 						}
 					}
 					select = false;
-					if(game.isWin()) view.popWin();	
+					if (game.isWin()) {
+						view.popWin();	
+					}
 				}
 			}
 		});
@@ -60,15 +70,53 @@ public class Controller {
 	
 	//row1, col1 of original position and row2, col2 of destination
 	private Direction getDirec(int row1, int col1, int row2, int col2) {
-		if(row1!=row2 && col1 != col2) return null;
-		if(row1 == row2 && col1 == col2) return null;
-		if(row1 == row2) {
-			if(col1 > col2) return Direction.WEST;
-			else return Direction.EAST;
+		if (row1 != row2 && col1 != col2) {
+			return null;
 		}
-		else if(col1 == col2){
-			if(row1 > row2) return Direction.NORTH;
-			else return Direction.SOUTH;
+		if (row1 == row2 && col1 == col2) {
+			return null;
+		}
+		if (row1 == row2) {
+			if (col1 > col2) {
+				return Direction.WEST;
+			} else {
+				return Direction.EAST;
+			}
+		} else if(col1 == col2){
+			if(row1 > row2) {
+				return Direction.NORTH;
+			} else {
+				return Direction.SOUTH;
+			}
+		}
+		return null;
+	}
+	
+	//row1, col1 of original position and row2, col2 of destination
+	private Direction getDirection(Point source, Point dest) {
+		// Check if new destination is in line with the source
+		if (source.getY() != dest.getY() && source.getX() != dest.getX()) {
+			return null;
+		}
+		
+		// Check if the destination is the same point as the source
+		if (source.equals(dest)) {
+			return null;
+		}
+		
+		// If source and dest are on same row
+		if ((int) source.getY() == (int) dest.getY()) {
+			if (source.getX() > dest.getX()) {
+				return Direction.WEST;
+			} else {
+				return Direction.EAST;
+			}
+		} else if ((int) source.getX() == (int) dest.getX()) { // If source and dest are in same column
+			if (source.getY() > dest.getY()) {
+				return Direction.NORTH;
+			} else {
+				return Direction.SOUTH;
+			}
 		}
 		return null;
 	}
--- Jump_In_Game/src/mvc/GridButton.java
+++ Jump_In_Game/src/mvc/GridButton.java
@@ -2,22 +2,76 @@ package mvc;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
 
 import javax.swing.JButton;
+import javax.swing.border.Border;
 
 public class GridButton extends JButton {
+	
 	private boolean isHole;
+	private PieceType pieceType;
+	private Point gridLocation;	// location in the grid of the button
+	
 	
 	public GridButton(int row, int col) {
+		this.gridLocation = new Point(col, row);
 		this.setPreferredSize(new Dimension(100, 100));
+		
+//		this.setBounds(new Rectangle(100, 100));
+//		this.setBorder(new RoundedBorder(50));
+//		this.setForeground(Color.BLUE);
+		
+		
 		
 		// Check if this gridbutton is supposed to be a hole
 		if (row == 0 || row == 4) {
 			if (col == 0 || col == 4) {
-				this.setBackground(new Color(139,69,19));
+				this.isHole = true;
 			}
 		} else if (row == 2 && col == 2) {
+			this.isHole = true;
+		} else {
+			this.isHole = false;
+		}
+		
+		if (this.isHole) {
 			this.setBackground(new Color(139,69,19));
 		}
 	}
+
+
+	public boolean isHole() {
+		return isHole;
+	}
+
+
+	public void setHole(boolean isHole) {
+		this.isHole = isHole;
+	}
+
+
+	public PieceType getPieceType() {
+		return pieceType;
+	}
+
+
+	public void setPieceType(PieceType pieceType) {
+		this.pieceType = pieceType;
+	}
+
+
+	public Point getGridLocation() {
+		return gridLocation;
+	}
+
+
+	public void setGridLocation(Point location) {
+		this.gridLocation = location;
+	}
+	
+	
+	
+	
 }
