b13514700c3287642d7995640c418db1e80f45ef
Mika Argyle <mika.argyle@gmail.com>
2019-11-04 19:29:34 -0500

    Update Direction.java

--- Jump_In_Game/src/gamePieces/Direction.java
+++ Jump_In_Game/src/gamePieces/Direction.java
@@ -1,5 +1,10 @@
 package gamePieces; 
 
+/** 
+ * The direction class contains all the possible directions for the gamepieces.
+ * @author Ruixuan Ni
+ */
+
 public enum Direction {
 	VERTICAL, HORIZONTAL, 
 	SOUTH, NORTH, EAST, WEST,
