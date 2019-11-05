885601b105913e17767cf5c4cfd2ff36ae91f08c
Mika Argyle <mika.argyle@gmail.com>
2019-11-04 19:31:41 -0500

    Update Rabbit.java

--- Jump_In_Game/src/gamePieces/Rabbit.java
+++ Jump_In_Game/src/gamePieces/Rabbit.java
@@ -1,5 +1,10 @@
 package gamePieces;
 
+/** 
+ * The rabbit class is used to create rabbits of a given name with given coordinates.
+ * @author Mika Argyle
+ */
+
 public class Rabbit extends Square{
 	
 	public Rabbit(int x, int y, String str) {
