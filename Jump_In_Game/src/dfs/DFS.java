f947bc2084188ec1bb27517284a076aabe04d0e3
Ruixuan Ni <nrx0320@outlook.com>
2019-11-16 22:53:46 -0500

    StackOverFlow

--- Jump_In_Game/src/dfs/DFS.java
+++ Jump_In_Game/src/dfs/DFS.java
@@ -17,7 +17,7 @@ public class DFS {
 	
 	public DFS(TilePlayBoard board) {
 		this.temp = new TilePlayBoard(board);
-		this.level = 6;
+		this.level = 3;
 		this.state = 0;
 		//this.undoNum = 0;
 		
@@ -39,7 +39,7 @@ public class DFS {
 		search(head);
 		
 		
-		getSolvent();		
+		//getSolvent();		
 	}
 
 	private void search(DFSNode node) {
@@ -52,6 +52,7 @@ public class DFS {
 				node.setNextNode();
 				temp.undoAndClear();
 				state--;
+				System.out.println("no other way to go, back");
 				search(node);
 			}
 		}
@@ -66,6 +67,7 @@ public class DFS {
 				if(point != null) {//can move rabbit
 					temp.moveRabbit(temp.getRabbit(piece), point);
 					if(node.hasOtherWayToGo()) {
+						//System.out.println("move rabbit"+piece + "to" + Direction.values()[direc]);
 						node.setNextNode();
 						state++;
 					}
@@ -73,11 +75,13 @@ public class DFS {
 						node = node.getPrev();
 						node.setNextNode();
 						temp.undoAndClear();
+						//System.out.println("move rabbit"+piece + "back from" + Direction.values()[direc]);
 					}
 				}
 				else {//unable to move rabbit
 					node = node.getPrev();
 					node.setNextNode();
+					//System.out.println("cannot move rabbit"+piece + "to" + Direction.values()[direc]);
 				}
 			}
 			else {//move fox
@@ -87,44 +91,50 @@ public class DFS {
 				if(orientation.equals(Direction.NORTH) || orientation.equals(Direction.SOUTH)) {
 					int col = temp.getFox(fox).getHead().getCol();
 					GridPoint point =  new GridPoint(direc, col);
-					//if(temp.testValidFoxMove(temp.getFox(fox), point)){
-					temp.moveFox(temp.getFox(fox), point);
-					if(node.hasOtherWayToGo()) {//can move fox and has next
-						state++;
-						node.setNextNode();
+					if(temp.testValidFoxMove(temp.getFox(fox), point)){
+						temp.moveFox(temp.getFox(fox), point);
+						if(node.hasOtherWayToGo()) {//can move fox and has next
+							state++;
+							node.setNextNode();
+							//System.out.println("move fox"+ fox + "to" + direc);
 							//cur = cur.getNext();
+						}
+						else {//can move fox but not has next
+							node = node.getPrev();
+							node.setNextNode();
+							temp.undoAndClear();
+							//System.out.println("move fox"+ fox + "back from" + direc);
+						}
 					}
-					else {//can move fox but not has next
+					else {//cannot move fox
 						node = node.getPrev();
 						node.setNextNode();
-						temp.undoAndClear();
+						//System.out.println("can not move fox"+ fox + "to" + direc);
 					}
-					//}
-					/*else {//cannot move fox
-						node = node.getPrev();
-						node.setNextNode();
-					}*/
 				}
 				else if(orientation.equals(Direction.EAST) || orientation.equals(Direction.WEST)) {
 					int row = temp.getFox(fox).getHead().getRow();
 					GridPoint point =  new GridPoint(row, direc);
-					//if(temp.testValidFoxMove(temp.getFox(fox), point)){
-					temp.moveFox(temp.getFox(fox), point);
-					if(node.hasOtherWayToGo()) {//can move fox and has next
-						state++;
-						node.setNextNode();
+					if(temp.testValidFoxMove(temp.getFox(fox), point)){
+						temp.moveFox(temp.getFox(fox), point);
+						if(node.hasOtherWayToGo()) {//can move fox and has next
+							state++;
+							node.setNextNode();
+							//System.out.println("move fox"+ fox + "to" + direc);
 							//cur = cur.getNext();
+						}
+						else {//can move fox but not has next
+							node = node.getPrev();
+							node.setNextNode();
+							temp.undoAndClear();
+							//System.out.println("move fox"+ fox + "to" + direc);
+						}
 					}
-					else {//can move fox but not has next
-						node = node.getPrev();
-						node.setNextNode();
-						temp.undoAndClear();
-					}
-					/*}
 					else {//cannot move fox
 						node = node.getPrev();
 						node.setNextNode();
-					}*/
+						//System.out.println("can not move fox"+ fox + "to" + direc);
+					}
 				}
 			}
 			search(node);
@@ -133,183 +143,12 @@ public class DFS {
 	}
 	
 	
-	/*private void DFSSearch() {
-		
-		if(temp.isWin())return;
-		
-		if(!cur.hasNext()) {
-			if(!cur.hasPrev()) return;
-			else {
-				cur = cur.getPrev();
-				cur.removeNext();
-				System.out.println("return & undo");
-				temp.undo();
-				DFSSearch();
-			}
-		}
-		
-		else {
-			cur = cur.getNext();
-			
-			int piece = cur.getName();
-			int direc = cur.getDirec();
-			
-			if(piece < temp.getRabbitNum()) {
-				GridPoint point = temp.getNearestJumpPoint(temp.getRabbit(piece), Direction.values()[direc]); 
-				if(point != null) {//can move rabbit
-					temp.moveRabbit(temp.getRabbit(piece), point);
-					if(cur.hasNext()) {
-						System.out.println("move rabbit" + piece + "," + Direction.values()[direc]);
-						//cur = cur.getNext();
-					}
-					else {
-						//System.out.println("undo rabbit");
-						cur = cur.getPrev();
-						cur.removeNext();
-						temp.undo();
-						System.out.println("no more next, undo and return to rabbit" + cur.getName() + ":" + Direction.values()[cur.getDirec()]);
-						/*cur = cur.getPrev();
-						cur.removeNext();
-						if(cur.hasNext()) {
-							System.out.println("undo rabbit" + piece);
-							cur = cur.getNext();
-							temp.undo();
-						}
-						else {
-							while(cur.hasPrev()) {
-								temp.undo();
-								System.out.println("undo rabbit" + piece);
-								cur = cur.getPrev();
-								if(cur.hasNext()) break;
-							}
-							cur = cur.getNext();
-						}
-					}
-				}
-				else {//unable to move rabbit
-					cur = cur.getPrev();
-					cur.removeNext();
-					System.out.println("cannot move rabbit" + piece + ",back");
-					/*if(cur.hasNext()) {
-						cur = cur.getNext();
-						//temp.undo();
-						//System.out.println("undo rabbit" + piece);
-					}
-					else {
-						while(cur.hasPrev()) {
-							temp.undo();
-							System.out.println("undo rabbit" + piece);
-							cur = cur.getPrev();
-							if(cur.hasNext()) break;
-						}
-						System.out.println(piece + "," + direc);
-						cur = cur.getNext();
-					}
-				}
-				
-			}
-			else {//move fox
-				int fox = piece-temp.getRabbitNum();
-				Direction orientation = temp.getFox(fox).getOrientation();
-				
-				if(orientation.equals(Direction.NORTH) || orientation.equals(Direction.SOUTH)) {
-					int col = temp.getFox(fox).getHead().getCol();
-					GridPoint point =  new GridPoint(direc, col);
-					if(temp.testValidFoxMove(temp.getFox(fox), point)){
-						temp.moveFox(temp.getFox(fox), point);
-						if(cur.hasNext()) {//can move fox and has next
-							System.out.println("move fox" + piece + "," + point.toString());
-							//cur = cur.getNext();
-						}
-						else {//can move fox but not has next
-							cur = cur.getPrev();
-							cur.removeNext();
-							System.out.println("undo fox move");
-							temp.undo();
-							/*if(cur.hasNext()) {
-								cur = cur.getNext();
-								temp.undo();
-								System.out.println("undo fox");
-							}
-							else {
-								while(cur.hasPrev()) {
-									temp.undo();
-									cur = cur.getPrev();
-									if(cur.hasNext()) break;
-								}
-								cur = cur.getNext();
-							}
-							
-						}
-					}
-					else {//cannot move fox
-						System.out.println("cannot move fox" + fox + "and back");
-						cur = cur.getPrev();
-						cur.removeNext();
-						/*if(cur.hasNext()) {
-							cur = cur.getNext();
-							//temp.undo();
-						}
-						else {
-							while(cur.hasPrev()) {
-								temp.undo();
-								cur = cur.getPrev();
-								System.out.println("undo fox");
-								if(cur.hasNext()) break;
-							}
-							cur = cur.getNext();
-						}
-					}
-				}
-				else if(orientation.equals(Direction.EAST) || orientation.equals(Direction.WEST)) {
-					int row = temp.getFox(fox).getHead().getRow();
-					GridPoint point =  new GridPoint(row, direc);
-					if(temp.testValidFoxMove(temp.getFox(fox), point)){
-						temp.moveFox(temp.getFox(fox), point);
-						if(cur.hasNext()) {//can move fox and has next
-							System.out.println("move fox" + piece +  "," + point.toString());
-							//cur = cur.getNext();
-						}
-						else {//can move fox but not has next
-							cur = cur.getPrev();
-							cur.removeNext();
-							System.out.println("undo fox move");
-							temp.undo();
-						}
-					}
-					else {//cannot move fox
-						System.out.println("cannot move fox" + fox + "and back");
-						cur = cur.getPrev();
-						cur.removeNext();
-						/*if(cur.hasNext()) {
-							cur = cur.getNext();
-							//temp.undo();
-						}
-						else {
-							while(cur.hasPrev()) {
-								temp.undo();
-								cur = cur.getPrev();
-								System.out.println("undo fox");
-								if(cur.hasNext()) break;
-							}
-							cur = cur.getNext();
-						}
-					}
-				}
-			}
-			DFSSearch();
-		}
-		
-		
-			
-	}*/
-	
 	public void getSolvent() {
-		Stack<Record> stack = temp.getList();
+		/*Stack<Record> stack = temp.getList();
 		while(!stack.isEmpty()) {
 			Record r = stack.pop();
 			System.out.println(r.getPiece().getName() + ":" + r.getNextLoc().getRow() + "," + r.getNextLoc().getCol());
-		}
+		}*/
 		System.out.println(temp.isWin());
 	}
 	
@@ -319,5 +158,4 @@ public class DFS {
 		solver.getSolvent();
 	}
 	
-
 }
