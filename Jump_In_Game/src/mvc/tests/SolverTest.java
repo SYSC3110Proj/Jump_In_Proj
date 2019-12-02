a8706da2373658aec66ab2594ad6c1e56078e859
Ruixuan Ni <nrx0320@outlook.com>
2019-12-02 16:43:21 -0500

    solverTest edit

--- Jump_In_Game/src/mvc/tests/SolverTest.java
+++ Jump_In_Game/src/mvc/tests/SolverTest.java
@@ -17,8 +17,7 @@ import mvc.controller.TilePlayBoard;
 public class SolverTest {
 
 	@Test
-	public void testFindSolution() {
-
+	public void testFindSolution1() {
 		TilePlayBoard board = new TilePlayBoard();
 		board.setMushroom(2, 3);
 		board.setMushroom(4, 2);
@@ -30,34 +29,33 @@ public class SolverTest {
 		Solver solve =  new Solver(board);
 		solve.findSolution();
 		assertEquals(true,solve.getWinState());
-		
-		
-		
-
+	}
+	
+	@Test
+	public void testFindSolution2() {
 		TilePlayBoard board1 = new TilePlayBoard();
-		board.setMushroom(2,3);
-		board.setMushroom(4,2);
-		board.setFox(new GridPoint(3,1),Direction.SOUTH);
-		board.setFox(new GridPoint(3,4),Direction.EAST);
-		board.setRabbit(1,3);
-		board.setRabbit(4,1);
+		board1.setMushroom(2,3);
+		board1.setMushroom(4,2);
+		board1.setFox(new GridPoint(3,1),Direction.SOUTH);
+		board1.setFox(new GridPoint(3,4),Direction.EAST);
+		board1.setRabbit(1,3);
+		board1.setRabbit(4,1);
 		Solver solve1 =  new Solver(board1);
 		solve1.findSolution();
 		assertEquals(true,solve1.getWinState());
+	}	
 		
-		
-		
+	@Test 
+	public void testFindSolution3() {
 		TilePlayBoard board2 = new TilePlayBoard();
-		board2.setMushroom(3,0);
+		board2.setMushroom(0,3);
 		board2.setMushroom(1,2);
-		board.setRabbit(0,1);
-		board.setRabbit(4,2);
+		board2.setMushroom(3,2);
+		board2.setRabbit(0,1);
+		board2.setRabbit(4,2);
 		Solver solve2 =  new Solver(board2);
 		solve2.findSolution();
 		assertEquals(true,solve2.getWinState());
-
-
-
 	}
 
 }
